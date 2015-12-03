d889d0df6ea02727f303267b706a7eba3fd5904e
Aina Lopez <aina.lopez@gmail.com>
2015-12-03 19:00:42 +0100

    Description of our project

--- web/index.php
+++ web/index.php
@@ -62,7 +62,10 @@ function show_content(optionsId) {
 			<h2>Home</h2>
 			<h3>The challenge</h3>
 			
-			<p>Changes. Every sales team seeks to identify and exploit opportunities to increase sales. In addition to acquiring new customers, another way to increase sales is to target existing customers by providing them with relevant offers that might persuade them to purchase additional items. In order to be able to do that, a sales team would need to be able to build up offers that customers truly value and to identify those customers that are more likely to purchase new items.</p>
+  <p> Odds reflect the likelihood of a a certain event. In football matches, betting companies define three odds for each match: home win odd, away win odd and draw odd. The lowest odd is the most likely result. Also, the odd represents the amount that a person is going to earn if the team that he bet for wins. 
+
+
+  Every sales team seeks to identify and exploit opportunities to increase sales. In addition to acquiring new customers, another way to increase sales is to target existing customers by providing them with relevant offers that might persuade them to purchase additional items. In order to be able to do that, a sales team would need to be able to build up offers that customers truly value and to identify those customers that are more likely to purchase new items.</p>
 			
 			<ul style="list-style-type:circle">
   				<li> The first objective can be achieved by developing a recommendation engine which, based on a customer's intended purchases can recommend additional items that are related to those already in the shopping basket. </li>
@@ -71,10 +74,11 @@ function show_content(optionsId) {
 			
 			<h3>The solution</h3>
 						
-			<p>We have addressed the above challenges in two steps:</p>
+			<p>We have addressed the above challenges in the following steps steps:</p>
 				<ul style="list-style-type:circle">
-					<li> First we have implemented a simple recommendation system, based on the <b><a href="http://www.wikipedia.org/wiki/Apriori_algorithm" target="_blank">Apriori algorithm.</a></b>. This algorithm analyses existing transaction recorded in the database and develops associative rules between products, based on the frequency of them being purchased together. The output of the algorithm is a set of rules which link a set of two or more products (which are assumed to be already in the customer's basket) with an additional product that the customer is recommended to buy.</li>
-					<li> To address the second part of the challenge, i.e. to identify the company's "most interesting revenue generating customers", we have carried out a LASSO Regression, on the subset of the 20 top clients measured by total revenue, regressing each customers purchases (measure in quantities) on the total revenues associated to each product. We have used the results of this regression to rank customers according to their <b>percentage monetary contribution</b> to total revenues from buying one additional product. We have used a <b>LASSO approach</b> to identify customers for personalised marketing campaigns, allowing the company to move away from a massive and unrestricted campaign to a more efficient use of marketing resources.</li>
+					<li> First we have implemented a simple recommendation engine, based on the <b><a href="https://en.wikipedia.org/wiki/Push–relabel_maximum_flow_algorithm" target="_blank">Push relabel algorithm.</a></b>. Although this algorithm finds the maximum flow it can be easily changed to find the maximum matching in bipartite graphs. The weights of the nodes used reflect the prediction success of each betting company predicting matches of each team. The output of the algorithm is a set of betting company and team pairs which can be interpreted as the teams whose matches are better predicted by each betting company. Then, when betting, the better option is to bet for the lowest betting odd in that match.</li>
+
+					<li> To address the second part of the challenge, BLABLABLA.</li>
 				</ul>
 						
 		</div>	
