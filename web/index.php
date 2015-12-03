2a4eb10308d0f0d1754b2cc1a2e38782cbb37443
Aina Lopez <aina.lopez@gmail.com>
2015-12-03 20:14:46 +0100

    Project description updated

--- web/index.php
+++ web/index.php
@@ -62,18 +62,18 @@ function show_content(optionsId) {
 			<h2>Home</h2>
 			<h3>The challenge</h3>
 			
-  <p> Odds reflect the likelihood of a a certain event. In football matches, betting companies define three odds for each match: home win odd, away win odd and draw odd.  Each odd represents the amount that a person is going to earn if the team for which he bet wins. Therefore, the lowest odd is the most likely result. In order to maximize the revenues winned when betting in football matches, we would like to find for which teams do we have to bet in each betting company. Another way to also maximize the amount of money winned when gambling, is to BLABLABLA.</p>
+  <p> Odds reflect the likelihood of a a certain event. In football matches, betting companies define three odds for each match: home win odd, away win odd and draw odd.  Each odd represents the amount that a person is going to earn if the team for which he bet wins. Therefore, the lowest odd is the most likely result. In order to maximize the revenues winned when betting in football matches, we would like to find which would be the most safe match to bet for in each betting company. Another way to also maximize the amount of money winned when gambling, is to BLABLABLA.</p>
 			
 			<ul style="list-style-type:circle">
-																																																						       <li> The first objective can be achieved by developing a recommendation engine which, based on betting companies prediction success in previous matches it is possible to recommend in which matches is more safe to bet.</li>
+																																																						       <li> The first objective can be achieved by developing a recommendation engine which, based on betting companies prediction success in previous matches it recommends what are the best matches to bet for in each betting company.</li>
   				<li> The second objective can be achieved by BLABLABLA.</li>
 			</ul>
 			
 			<h3>The solution</h3>
 						
-			<p>We have addressed the above challenges in the following steps steps:</p>
+			<p>We have addressed the above challenges in the following steps:</p>
 				<ul style="list-style-type:circle">
-					<li> First we have implemented a simple recommendation engine, based on the <b><a href="https://en.wikipedia.org/wiki/Push-relabel_maximum_flow_algorithm" target="_blank">Push relabel algorithm.</a></b>. Although this algorithm finds the maximum flow it can be easily changed to find the maximum matching in bipartite graphs. The weights of the nodes used reflect the prediction success of each betting company predicting matches of each team. The output of the algorithm is a set of betting company and team pairs which can be interpreted as the teams whose matches are better predicted by each betting company. Then, when betting, the better option is to bet for the lowest betting odd in that match.</li>
+  <li> First we have implemented two simple recommendation engines, based on the <b><a href="https://en.wikipedia.org/wiki/Push-relabel_maximum_flow_algorithm" target="_blank">Push relabel algorithm.</a></b>. Although this algorithm finds the maximum flow it can be easily changed to find the maximum matching in bipartite graphs. The difference between the two systems is the formula applied to compute the bets for each nodes: one model returns the most safe option and the other returns a more risky option with a higher revenue. This weights reflect the prediction success of each betting company predicting matches of each team. The output of the algorithms is a set of betting company and team pairs which can be interpreted as the teams whose matches are better predicted by each betting company. Then, when betting, the better option is to bet for the lowest betting odd in that match.</li>
 
 					<li> To address the second part of the challenge, BLABLABLA.</li>
 				</ul>
